98141c43b7c4d5fff912dda4d0d4f9a0eb93e667
Phemus <ramin.vazir@landregistry.gsi.gov.uk>
2014-12-09 15:00:03 +0000

    Bamdiddy Flandrake

--- flask-app.py
+++ flask-app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-	return 'Hello Frank The Tank!'
+	return 'Make Sam Pancakes!'
 	
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug = True)
